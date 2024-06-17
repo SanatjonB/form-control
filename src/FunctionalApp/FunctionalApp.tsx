10b86a8b5bbf7714454a911fda4d3215b7a14d53
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2024-06-16 21:29:00 -0400

    states

--- src/FunctionalApp/FunctionalApp.tsx
+++ src/FunctionalApp/FunctionalApp.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { ProfileInformation } from "../ProfileInformation";
 import { FunctionalForm } from "./FunctionalForm";
 
 export const FunctionalApp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [city, setCity] = useState("");
   return (
     <>
       <h2>Functional</h2>
